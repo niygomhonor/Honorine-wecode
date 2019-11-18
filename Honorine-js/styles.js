91769dc3898f4e2c9838242a227b88ca71aa7c35
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-11-18 21:10:27 +0200

    Add branch

---
+++ Honorine-js/styles.js
@@ -0,0 +1,8 @@
+function myDesign(){
+        var name= document.getElementById("fullName").value;
+    
+        if (name !==" ") {
+        alert(document.getElementById("fullName").innerHTML =name
+         +", we have received your message. Thank you fro reaching out to me.")}   
+
+}
